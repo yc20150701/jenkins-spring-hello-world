6c1f615985d5cc6084313f8a6d488d268546d002
yc20150701 <106296569+yc20150701@users.noreply.github.com>
2023-02-22 15:48:30 +0800

    Update HelloController.java

--- src/main/java/com/example/jenkinsspringhelloworld/controller/HelloController.java
+++ src/main/java/com/example/jenkinsspringhelloworld/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("hello")
     public String hello(){
-        return "hello jenkins by spring boot";
+        return "hello jenkins by spring boot change";
     }
 }
