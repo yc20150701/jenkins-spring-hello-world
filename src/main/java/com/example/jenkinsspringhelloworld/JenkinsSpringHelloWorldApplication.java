cf03489d96c86b1476b4473cc105b1a1f9a114c3
yc20150701 <yc20150701@gmail.com>
2023-02-22 10:48:52 +0800

    spring boot

---
+++ src/main/java/com/example/jenkinsspringhelloworld/JenkinsSpringHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsspringhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSpringHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsSpringHelloWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkinsspringhelloworld/JenkinsSpringHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsspringhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSpringHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
