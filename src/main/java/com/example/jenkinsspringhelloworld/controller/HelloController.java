ad2cc36b8161869931298e692d5bb1690e36ef7e
yc20150701 <106296569+yc20150701@users.noreply.github.com>
2023-05-09 11:59:01 +0800

    Update HelloController.java

--- src/main/java/com/example/jenkinsspringhelloworld/controller/HelloController.java
+++ src/main/java/com/example/jenkinsspringhelloworld/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.example.jenkinsspringhelloworld.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.UUID
 
 @RestController
 public class HelloController {
