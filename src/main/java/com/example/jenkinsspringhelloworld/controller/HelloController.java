9dc965b9af2db269de2b180abe7722a5bdb31883
yc20150701 <106296569+yc20150701@users.noreply.github.com>
2023-05-09 11:59:46 +0800

    Update HelloController.java

--- src/main/java/com/example/jenkinsspringhelloworld/controller/HelloController.java
+++ src/main/java/com/example/jenkinsspringhelloworld/controller/HelloController.java
@@ -2,14 +2,13 @@ package com.example.jenkinsspringhelloworld.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.UUID
+
 
 @RestController
 public class HelloController {
 
     @GetMapping("hello")
     public String hello(){
-        System.out.println(UUID.randomUUID().toString());
         return "hello jenkins by spring boot change 5/9";
     }
 }
