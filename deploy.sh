edeb0ba87e2bf4d8f8b56b5009d872a604d612bc
yc20150701 <106296569+yc20150701@users.noreply.github.com>
2023-05-09 10:36:50 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cd ./jenkins-spring-hello-world/target
-nohup java -jar jenkins-spring-hello-world-0.0.1-SNAPSHOT.jar &>mylog.log &
+#cd ./jenkins-spring-hello-world/target
+#nohup java -jar jenkins-spring-hello-world-0.0.1-SNAPSHOT.jar &>mylog.log &
+echo '111111111111'
